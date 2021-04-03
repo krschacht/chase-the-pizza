9173126d46744b07ff29678cd8c0122be3593dd2
Keith Schacht <krschacht@gmail.com>
2021-04-03 15:52:17 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,6 +3,23 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . 2 . . . . 2 . . 
+. . 2 . . . . 2 . . 
+. . 2 . . . . 2 . . 
+. . 2 . . . . 2 . . 
+. . 2 . . . . 2 . . 
+2 2 2 . . . . 2 2 2 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+        }
+        return null;
+    })
+
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
